03326a6f36db6ee5c08198b514ed41c7dc9b9a24
Akshita <aimachiever07@gmail.com>
2025-05-19 22:19:25 +0530

    Add files via upload

---
+++ 02_task_print.py
@@ -0,0 +1,2 @@
+# task 1 fun fact generator
+print("you know Asians think cricket is no.1 sport in the world!!\nsachin tendulkar got out 200 times because of worng umpiring decision.\nEvery year 1 lakh new people start to practice cricket but on the other side 80 thousand leave cricket")
